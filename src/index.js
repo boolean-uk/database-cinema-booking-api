1c650bd6af0971417883b333c5436b1f81e3f157
Nathan King <nathanking3590@gmail.com>
2022-01-24 17:26:56 +0000

    Add comments for clarity

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+// Load our .env file
 require('dotenv').config();
 
+// Import express and cors
 const express = require('express');
 const cors = require('cors');
 
+// Set up express
 const app = express();
-
 app.disable('x-powered-by');
-
 app.use(cors());
+// Tell express to use a JSON parser middleware
 app.use(express.json());
+// Tell express to use a URL Encoding middleware
 app.use(express.urlencoded({ extended: true }));
+
+
 
 
 
@@ -21,12 +26,15 @@ app.use('/customer', customerRouter);
 
 
 
+
+// Set up a default "catch all" route to use when someone visits a route
+// that we haven't built
 app.get('*', (req, res) => {
     res.json({ ok: true });
 });
 
+// Start our API server
 const port = process.env.PORT || 4000;
-
 app.listen(port, () => {
     console.log(`\n Server is running on http://localhost:${port}\n`);
-})
+});
