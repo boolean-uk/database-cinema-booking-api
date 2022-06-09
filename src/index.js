0cd4bc0dbdf87c7ff29b8c46031c0a9afaed8665
dearshrewdwit <dearshrewdwit@gmail.com>
2022-06-09 09:27:12 +0100

    modularise app

--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
 // Load our .env file
 require('dotenv').config();
-
-// Import express and cors
-const express = require('express');
-const cors = require('cors');
-
-// Set up express
-const app = express();
-app.disable('x-powered-by');
-app.use(cors());
-// Tell express to use a JSON parser middleware
-app.use(express.json());
-// Tell express to use a URL Encoding middleware
-app.use(express.urlencoded({ extended: true }));
-
-
-
-
-
-// Tell express to use your routers here
-const customerRouter = require('./routers/customer');
-app.use('/customer', customerRouter);
-
-
-
-
-
-
-// Set up a default "catch all" route to use when someone visits a route
-// that we haven't built
-app.get('*', (req, res) => {
-    res.json({ ok: true });
-});
-
+// Load app
+const app = require('./server.js')
+// Set the port
+const port = process.env.PORT || 4040;
 // Start our API server
-const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`\n Server is running on http://localhost:${port}\n`);
 });
