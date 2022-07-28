e09f1eea6a95af03e8c9af2c8f1b63ccd282dc64
Nathan King <nathanking3590@gmail.com>
2022-07-28 10:41:03 +0100

    add test helpers

---
+++ test/helpers/createCustomer.js
@@ -0,0 +1,19 @@
+const prisma = require("../../src/utils/prisma")
+
+const createCustomer = async (name, phone, email) => {
+  return await prisma.customer.create({
+    data: {
+      name: name,
+      contact: {
+        create: {
+          phone: phone,
+          email: email,
+        },
+      },
+    },
+  })
+}
+
+module.exports = {
+    createCustomer
+}
