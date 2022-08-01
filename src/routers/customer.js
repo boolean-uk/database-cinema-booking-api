db106ae57e9bdea669c184d4b781eaa00c118ec1
Lewis <ljoncb@gmail.com>
2022-08-01 15:38:01 +0100

    update customer by id

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const {
-    createCustomer
-} = require('../controllers/customer');
+  createCustomer,
+  updateCustomerByID,
+} = require("../controllers/customer");
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 // The below /register route extends that, so the end result will be a URL
 // that looks like http://localhost:4000/customer/register
 router.post("/register", createCustomer);
+router.put("/:id", updateCustomerByID);
 
 module.exports = router;
