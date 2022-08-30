df2767e8e2d6b12e444a58689f36303ee6f4f2c9
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-08-30 12:52:24 +0100

    adding controllers, routers, and server update, and get all for movie exercise

---
+++ src/routers/movie.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+    createMovie,
+    getAllMovies
+} = require('../controllers/movie');
+
+const router = express.Router();
+
+// In index.js, we told express that the /customer route should use this router file
+// The below /register route extends that, so the end result will be a URL
+// that looks like http://localhost:4000/customer/register
+router.get("/", getAllMovies);
+router.post("/register", createMovie);
+
+module.exports = router;
---
+++ src/routers/screen.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+    createScreen
+} = require('../controllers/screen');
+
+const router = express.Router();
+
+// In index.js, we told express that the /customer route should use this router file
+// The below /register route extends that, so the end result will be a URL
+// that looks like http://localhost:4000/customer/register
+router.post("/register", createScreen);
+
+module.exports = router;
