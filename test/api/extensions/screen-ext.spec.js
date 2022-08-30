0597df7ee1148ed34d61aa9e6c0a97938411ea05
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-30 16:44:03 +0100

    test: tests for screen extension complete

--- test/api/extensions/screen-ext.spec.js
+++ test/api/extensions/screen-ext.spec.js
@@ -1,6 +1,7 @@
 const supertest = require('supertest');
 const app = require('../../../src/server.js');
 const { createMovie } = require('../../helpers/createMovie.js');
+const { createScreen } = require('../../helpers/createScreen.js');
 
 describe('Screens Endpoint', () => {
   describe('POST /screens', () => {
@@ -44,6 +45,27 @@ describe('Screens Endpoint', () => {
       expect(response.body.screen).not.toEqual(undefined);
       expect(response.body.screen.number).toEqual(11);
       expect(response.body.screen.screenings.length).toEqual(3);
+    });
+
+    it('will return 400 status', async () => {
+      const request = {};
+
+      const response = await supertest(app).post('/screens').send(request);
+
+      expect(response.status).toEqual(400);
+      expect(response.body.error).toEqual('Missing fields in request body');
+    });
+
+    it('will return 409 status', async () => {
+      await createScreen(1);
+      const request = { number: 1 };
+
+      const response = await supertest(app).post('/screens').send(request);
+
+      expect(response.status).toEqual(409);
+      expect(response.body.error).toEqual(
+        'A screen with the provided number already exists'
+      );
     });
   });
 });
