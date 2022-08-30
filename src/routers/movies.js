7ef715a3d3bcd7129549437c0cdace2a4bbbdc29
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-08-30 18:17:18 +0100

    standard

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getMovies, createMovie } = require("../controllers/movies");
+const {
+  getMovies,
+  createMovie,
+  getMovieByID,
+  updateMovie,
+} = require("../controllers/movies");
 
 router.get("/", getMovies);
 
 router.post("/", createMovie);
+
+router.get("/:id", getMovieByID);
+
+router.put("/:id", updateMovie);
 
 module.exports = router;
