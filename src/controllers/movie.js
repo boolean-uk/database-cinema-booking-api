ebe488526d97aa9965548c8c2c83be2190e918e7
John Xander <johnblox5@gmail.com>
2022-08-31 10:58:06 +0100

    Finally fixed the bug

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -4,18 +4,23 @@ const prisma = require('../utils/prisma')
 // Get all movies
 const getAllMovies = async (req, res) => {
     try {
-        const allMovies = await prisma.movie.findMany({
-            where: {
-                AND: [Object.keys(req.query)[0] === "runtimeLt" ?
-                    { runtimeMins: { lte: +req.query.runtimeLt } } :
-                    { runtimeMins: { gte: +req.query.runtimeGt } }
-                ]
-            },
-            include: {
-                screenings: true,
-            }
-        })
-        res.status(200).json({ movies: allMovies })
+        if (Object.keys(req.query).length === 0) {
+            const allMovies = await prisma.movie.findMany({ include: { screenings: true } })
+            res.status(200).json({ movies: allMovies })
+        } else {
+            const allMovies = await prisma.movie.findMany({
+                where: {
+                    AND: [Object.keys(req.query)[0] === "runtimeLt" ?
+                        { runtimeMins: { lte: +req.query.runtimeLt } } :
+                        { runtimeMins: { gte: +req.query.runtimeGt } }
+                    ]
+                },
+                include: {
+                    screenings: true,
+                }
+            })
+            res.status(200).json({ movies: allMovies })
+        }
     } catch (err) {
         res.status(404).json({ error: err })
     }
@@ -25,9 +30,13 @@ const getAllMovies = async (req, res) => {
 const createMovie = async (req, res) => {
     try {
         const createdMovie = await prisma.movie.create({
-            data: {
+            data: req.body.screenings === undefined ? {
                 title: req.body.title,
                 runtimeMins: req.body.runtimeMins
+            } : {
+                title: req.body.title,
+                runtimeMins: req.body.runtimeMins,
+                screenings: req.body.screenings
             },
             include: {
                 screenings: true,
