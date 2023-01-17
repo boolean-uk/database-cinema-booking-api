138a6ed8a8a44fa87c7757cdbeb4bfd9621c491b
MattVwaves <jamesmattholt@gmail.com>
2023-01-17 11:40:01 +0000

    create movie underway!

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -8,4 +8,33 @@ const getAllMovies = async (req, res) => {
   } catch (error) {}
 };
 
-module.exports = { getAllMovies };
+const createMovie = async (req, res) => {
+  const { title, runtimeMins } = req.body;
+
+  if (!title || !runtimeMins) {
+    return res.status(400).json({
+      error: "Missing fields in request body",
+    });
+  }
+
+  try {
+    const createdMovie = await prisma.movie.create({
+      data: {
+        title,
+        runtimeMins,
+      },
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return res
+          .status(409)
+          .json({ error: "A movie with the provided title already exists" });
+      }
+    }
+
+    res.status(500).json({ error: e.message });
+  }
+};
+
+module.exports = { getAllMovies, createMovie };
--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllMovies } = require("../controllers/movies");
+const { getAllMovies, createMovie } = require("../controllers/movies");
 
 const router = express.Router();
 
 router.get("/", getAllMovies);
+router.post("/", createMovie)
 
 module.exports = router;
