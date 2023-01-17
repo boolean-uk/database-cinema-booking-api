f77f111ba6cd3a55ccb876292f67de6a1d61107f
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-17 16:45:53 +0000

    post for creating screen completed

--- src/server.js
+++ src/server.js
@@ -1,22 +1,25 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const cors = require("cors");
-const morgan = require("morgan");
+const cors = require('cors')
+const morgan = require('morgan')
 
-app.disable("x-powered-by");
+app.disable('x-powered-by')
 
 // Add middleware
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Tell express to use your routers here
-const customerRouter = require("./routers/customer");
-app.use("/customers", customerRouter);
+const customerRouter = require('./routers/customer')
+app.use('/customers', customerRouter)
 
-const movieRouter = require("./routers/movie");
-app.use("/movies", movieRouter);
+const movieRouter = require('./routers/movie')
+app.use('/movies', movieRouter)
 
-module.exports = app;
+const screenRouter = require('./routers/screens')
+app.use('/screens', screenRouter)
+
+module.exports = app
