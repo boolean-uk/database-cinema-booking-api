d5132ebc4ec4823a04bb48058cf66110e0fcd571
Dave <Daverevell491@gmail.com>
2023-01-18 11:09:25 +0000

    Attempting to populate screenings when creating a movie

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -2,98 +2,112 @@ const { Prisma } = require("@prisma/client");
 const prisma = require("../utils/prisma");
 
 const getMovie = async (req, res) => {
-  try {
-    const getAllMovies = await prisma.movie.findMany();
-    res.json({ data: getAllMovies });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+	try {
+		const getAllMovies = await prisma.movie.findMany();
+		res.json({ movies: getAllMovies, screenings: { create: screenings } });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 const createMovie = async (req, res) => {
-  const { title, runtimeMins } = req.body;
+	const { title, runtimeMins, screenings } = req.body;
 
-  if (!title || !runtimeMins) {
-    return res.status(400).json({
-      error: "Missing fields in request body",
-    });
-  }
+	const data = { title, runtimeMins };
 
-  try {
-    const createdMovie = await prisma.movie.create({
-      data: {
-        title,
-        runtimeMins,
-      },
-    });
-    res.status(201).json({ data: createdMovie });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+	if (screenings) {
+		console.log(screenings);
+		console.log(new Date());
+		data.screenings = {
+			create: screenings.map((screening) => ({
+				...screening,
+				// startsAt: new Date(screening.startsAt),
+			})),
+		};
+	}
+
+	if (!title || !runtimeMins) {
+		return res.status(400).json({
+			error: "Missing fields in request body",
+		});
+	}
+
+	try {
+		const createdMovie = await prisma.movie.create({
+			data,
+			include: {
+				screenings: true,
+			},
+		});
+
+		res.status(201).json({ movie: createdMovie });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 const getByID = async (req, res) => {
-  const id = Number(req.params.id);
+	const id = Number(req.params.id);
 
-  // Error handling for if a number isn't given
-  if (!id) {
-    return res.status(400).json({
-      error: "Given ID is not a number",
-    });
-  }
-  if (id === null) {
-    return res.status(400).json({
-      error: "Given ID can not be found",
-    });
-  }
-  try {
-    const getMovieByID = await prisma.movie.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    res.json({ data: getMovieByID });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+	// Error handling for if a number isn't given
+	if (!id) {
+		return res.status(400).json({
+			error: "Given ID is not a number",
+		});
+	}
+	if (id === null) {
+		return res.status(400).json({
+			error: "Given ID can not be found",
+		});
+	}
+	try {
+		const getMovieByID = await prisma.movie.findUnique({
+			where: {
+				id: id,
+			},
+		});
+		res.json({ movie: getMovieByID });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 const updateByID = async (req, res) => {
-  const id = Number(req.params.id);
-  const { title, runtimeMins } = req.body;
+	const id = Number(req.params.id);
+	const { title, runtimeMins } = req.body;
 
-  // Error handling for if a number isn't given
-  if (!id) {
-    return res.status(400).json({
-      error: "Given ID is not a number",
-    });
-  }
+	// Error handling for if a number isn't given
+	if (!id) {
+		return res.status(400).json({
+			error: "Given ID is not a number",
+		});
+	}
 
-  if (!title || !runtimeMins) {
-    return res.status(400).json({
-      error: "Missing fields in request body",
-    });
-  }
+	if (!title || !runtimeMins) {
+		return res.status(400).json({
+			error: "Missing fields in request body",
+		});
+	}
 
-  try {
-    const updateMovieByID = await prisma.movie.update({
-      where: {
-        id: id,
-      },
-      data: {
-        title,
-        runtimeMins,
-      },
-    });
-    res.json({ data: updateMovieByID });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+	try {
+		const updateMovieByID = await prisma.movie.update({
+			where: {
+				id: id,
+			},
+			data: {
+				title,
+				runtimeMins,
+			},
+		});
+		res.status(201).json({ movie: updateMovieByID });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 module.exports = {
-  getMovie,
-  createMovie,
-  getByID,
-  updateByID,
+	getMovie,
+	createMovie,
+	getByID,
+	updateByID,
 };
--- src/controllers/screens.js
+++ src/controllers/screens.js
@@ -1,26 +1,26 @@
-const { Prisma } = require('@prisma/client')
-const prisma = require('../utils/prisma')
+const { Prisma } = require("@prisma/client");
+const prisma = require("../utils/prisma");
 
 const createScreen = async (req, res) => {
-  const { number } = req.body
+	const { number } = req.body;
 
-  if (!number) {
-    return res.status(400).json({
-      error: 'you havent filled in the correct request body'
-    })
-  }
-  try {
-    const createdScreen = await prisma.screen.create({
-      data: {
-        number
-      }
-    })
-    res.status(201).json({ data: createdScreen })
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-}
+	if (!number) {
+		return res.status(400).json({
+			error: "you havent filled in the correct request body",
+		});
+	}
+	try {
+		const createdScreen = await prisma.screen.create({
+			data: {
+				number,
+			},
+		});
+		res.status(201).json({ screen: createdScreen });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
 
 module.exports = {
-  createScreen
-}
+	createScreen,
+};
