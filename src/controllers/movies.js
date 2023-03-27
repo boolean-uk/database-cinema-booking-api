8e9b01d10f7a4f16e7f8ae5d3bb4eefe4d7e777d
Kat De Mey <katdemey@outlook.com>
2023-03-27 12:37:51 +0100

    extensions complete. TODO: extensions to the extensions

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -20,7 +20,8 @@ const createMovie = async (req, res) => {
     });
   }
 
-  const createdMovie = await prisma.movie.create({
+  try {
+const createdMovie = await prisma.movie.create({
     data: {
       title,
       runtimeMins,
@@ -31,7 +32,17 @@ const createMovie = async (req, res) => {
   });
 
   return res.status(201).json({ movie: createdMovie });
+  }
+  catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return res
+          .status(409)
+          .json({ error: "A movie with tthis title already exists" });
+      } 
+    }
 };
+}
 
 const getMovieByID = async (req, res) => {
   const id = Number(req.params.id);
@@ -43,29 +54,57 @@ const getMovieByID = async (req, res) => {
       screenings: true,
     },
   });
+      if (!movie){
+      return res.status(404).json("movie with this id not found");
 
-  return res.status(200).json({ movie });
+      }
+      return res.status(200).json({ movie });
+
 };
 
 const updateMovieByID = async (req, res) => {
   const id = Number(req.params.id);
   const { title, runtimeMins } = req.body;
+   if ( !title ||!runtimeMins) {
+     return res.status(400).json({
+       error: "Missing fields in request body",
+     });
+   }
 
-  const movie = await prisma.movie.update({
-    where: {
-      id: id,
-    },
-    data: {
-      title,
-      runtimeMins,
-    },
-    include: {
-      screenings: true,
-    },
-  });
+   try {
+       const movie = await prisma.movie.update({
+         where: {
+           id: id,
+         },
+         data: {
+           title,
+           runtimeMins,
+         },
+         include: {
+           screenings: true,
+         },
+       });
 
-  return res.status(201).json({ movie });
+       return res.status(201).json({ movie });
+  
+   } catch (e) {
+     if (e instanceof Prisma.PrismaClientKnownRequestError) {
+       if (e.code === "P2025") {
+         return res
+           .status(404)
+           .json({ error: "movie with that id does not exist" });
+       }
+        if (e.code === "P2002") {
+          return res.status(409).json({
+            error: "Movie with that title already exists",
+          });
+        }
+          res.status(500).json({ error: e.message });
+     }
+     res.status(404).json({ error: e.message });
+   }  
 };
+
 
 module.exports = {
   getMovies,
