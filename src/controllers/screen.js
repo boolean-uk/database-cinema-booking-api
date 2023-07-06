b083e80623775ff2338a0695b43ec20f630a2eb2
carolina arruda <carolinacalouroarruda@gmail.com>
2023-07-06 18:25:25 +0200

    finished screen with screenings update

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -7,34 +7,37 @@ const getAll = async (req, res) => {
 };
 
 const createScreen = async (req, res) => {
-  let { number } = req.body;
+  const { number, screenings } = req.body;
 
   if (!number) {
     return res.status(400).json({
       error: "Missing fields in request body",
     });
-  } else if (number) {
-    number = Number(number);
-    const findNumber = await prisma.screen.findFirst({
-      where: { number: number },
-    });
-    if (findNumber) {
-      return res.status(409).json({
-        error: "A screen with the provided number already exists",
-      });
-    } else {
-      const createScreen = await prisma.screen.create({
-        data: {
-          number,
-        },
-        include: {
-          screenings: true,
-        },
-      });
-
-      res.status(201).json({ screen: createScreen });
-    }
   }
+
+  const existingScreen = await prisma.screen.findFirst({
+    where: { number: Number(number) },
+  });
+
+  if (existingScreen) {
+    return res.status(409).json({
+      error: "A screen with the provided number already exists",
+    });
+  }
+
+  const createdScreen = await prisma.screen.create({
+    data: {
+      number: Number(number),
+      screenings: {
+        create: screenings,
+      },
+    },
+    include: {
+      screenings: true,
+    },
+  });
+
+  res.status(201).json({ screen: createdScreen });
 };
 
 module.exports = {
--- test/api/extensions/screen.spec.js
+++ test/api/extensions/screen.spec.js
@@ -1,6 +1,7 @@
 const supertest = require("supertest");
 const app = require("../../../src/server.js");
 const { createScreen } = require("../../helpers/createScreen.js");
+const { createMovie } = require("../../helpers/createMovie.js");
 
 describe("Screen Endpoint", () => {
   describe("POST /screens", () => {
@@ -23,6 +24,27 @@ describe("Screen Endpoint", () => {
 
       expect(response.status).toEqual(409);
       expect(response.body).toHaveProperty("error");
+    });
+    it("will create a new screen with screening properties", async () => {
+      const screen = await createScreen(1);
+     const movie = await createMovie("Dodgeball", 120, screen);
+      
+      const request = {
+        number: 10,
+        screenings: [
+          {
+            movieId: movie.id,
+            startsAt: "2030-12-21T12:30:00.000Z"
+          },
+        ],
+      };
+
+      const response = await supertest(app).post("/screens").send(request);
+
+      expect(response.status).toEqual(201);
+      expect(response.body.screen).toHaveProperty("screenings");
+      expect(response.body.screen.screenings).not.toEqual(undefined);
+      expect(response.body.screen.screenings.length).toEqual(1);
     });
   });
 });
