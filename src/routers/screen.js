6b6fec41ef22ef11cd082504ae2bfe6713309127
rockettdev <kjr.dev@hotmail.com>
2023-07-08 18:31:07 +0100

    completed core

---
+++ src/routers/screen.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+    createScreen
+} = require('../controllers/screen');
+
+const router = express.Router();
+
+// In index.js, we told express that the /customer route should use this router file
+// The below /register route extends that, so the end result will be a URL
+// that looks like http://localhost:4000/customer/register
+
+router.post("/", createScreen);
+
+
+module.exports = router;
