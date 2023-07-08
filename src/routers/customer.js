7fb4316acb622fd1ed266276680c66bd45711400
rockettdev <kjr.dev@hotmail.com>
2023-07-08 18:10:12 +0100

    completed customer section

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-    createCustomer
+    createCustomer, updateCustomer
 } = require('../controllers/customer');
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 // In index.js, we told express that the /customer route should use this router file
 // The below /register route extends that, so the end result will be a URL
 // that looks like http://localhost:4000/customer/register
-router.post("/register", createCustomer);
+router.post("/register", createCustomer)
+router.put("/:id", updateCustomer);
 
 module.exports = router;
