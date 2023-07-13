7769873b9435f7c183ef94e5f24c039a6bf579f1
davidbrockway <dav.brock@protonmail.com>
2023-07-13 09:32:15 +0100

    Update

--- test/api/routes/movies.spec.js
+++ test/api/routes/movies.spec.js
@@ -90,21 +90,16 @@ describe("Movies Endpoint", () => {
 
     describe("GET /movies with runtime filters", () => {
         it("can filter movies by runtime", async () => {
-            const screen = await createScreen(1)
-            await createMovie('Dodgeball', 120, screen)
-            await createMovie('Scream', 90, screen)
-
-            const response = await supertest(app).get('/movies?runtimeLt=120&runtimeGt=90');
-
-            expect(response.status).toEqual(200)
-            expect(response.body.movies).not.toEqual(undefined)
-            expect(response.body.movies.length).toEqual(1)
-
-            const [movie] = response.body.movies
-            expect(movie.title).toEqual('Dodgeball')
-            expect(movie.runtimeMins).toEqual(120)
-            expect(movie.screenings).not.toEqual(undefined)
-            expect(movie.screenings.length).toEqual(1)
-        })
-    });
+          const screen = await createScreen(1);
+          await createMovie('Dodgeball', 120, screen);
+          await createMovie('Scream', 90, screen);
+          await createMovie('The Shawshank Redemption', 120, screen);
+      
+          const response = await supertest(app).get('/movies?runtimeLt=120&runtimeGt=90');
+      
+          expect(response.status).toBe(200);
+          expect(response.body.movies.length).toBe(1);
+          expect(response.body.movies[0].title).toBe('The Shawshank Redemption');
+        });
+      });
 })
