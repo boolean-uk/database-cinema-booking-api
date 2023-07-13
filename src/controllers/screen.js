2a70a99611f6c660cd96b97318911d9a097a26e2
davidbrockway <dav.brock@protonmail.com>
2023-07-13 04:47:22 +0100

    Extention API Spec Screen.js

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -1,26 +1,37 @@
-const { Prisma } = require('@prisma/client');
-const prisma = require('../utils/prisma');
+const { Prisma } = require("@prisma/client")
+const prisma = require('../utils/prisma')
 
-const createScreen = async (req, res) => {
-  const { number } = req.body;
+const updateCustomer = async (req, res) => {
+    const { id } = req.params;
+    const { name, contact } = req.body;
 
-  if (!number) {
-    return res.status(400).json();
-  }
+    if (!name) {
+        return res.status(400).json({ error: 'Missing fields in request body' });
+    }
 
-  try {
-    const createdScreen = await prisma.screen.create({
-      data: {
-        number,
-      },
-    });
+    try {
+        const updatedCustomer = await prisma.customer.update({
+            where: { id: parseInt(id) },
+            data: {
+                name,
+                contact: {
+                    update: contact,
+                },
+            },
+            include: {
+                contact: true,
+            },
+        });
 
-    res.status(201).json({ screen: createdScreen });
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
+        res.status(201).json({ customer: updatedCustomer });
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2025') {
+            return res.status(404).json({ error: 'Customer with that id does not exist' });
+        }
+        res.status(500).json({ error: e.message });
+    }
 };
 
 module.exports = {
-  createScreen,
+    updateCustomer,
 };
