7035df3731f4d632130220bc3d359483501c7886
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 16:22:59 +0000

    FEAT: added createScreen() controller.

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -1,7 +1,12 @@
-// Write controllers here
+const { createScreenDB } = require('../domains/screen.js')
 
-
+const createScreen = async (req, res) => {
+    const { number } = req.body
+    const newScreen = await createScreenDB(number)
+    console.log(newScreen)
+    res.status(201).json({screen: newScreen})
+}
 
 module.exports = {
-    //  functions for router
+    createScreen,
 }
