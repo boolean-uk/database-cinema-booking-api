a740ee060326c06a8cd6399caa94abffdba63c2d
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-16 13:14:27 +0000

    feat: add typedefs

--- src/utils/types.d.js
+++ src/utils/types.d.js
@@ -2,6 +2,9 @@
  * @typedef {import("express").Request} Request
  * @typedef {import("express").Response} Response
  * @typedef {import("express").NextFunction} NextFunction
+ * 
+ * @typedef {import("@prisma/client").Customer} Customer
+ * @typedef {import("@prisma/client").Contact} Contact
  */
 const Types = {};
 
