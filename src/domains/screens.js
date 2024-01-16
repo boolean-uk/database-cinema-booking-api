b8d26343a0484f79de2d92abeba46c5cdbd52e29
Kye <NovusAa12@proton.me>
2024-01-16 15:27:55 +0000

    add routes for screens

---
+++ src/domains/screens.js
@@ -0,0 +1,35 @@
+const prisma = require("../utils/prisma");
+
+const getAllScreensDb = async () => await prisma.screen.findMany();
+
+const createScreenDb = async (request_body) => {
+  const { number, screenings } = request_body;
+
+  let dataToCreate = {};
+
+  if (number) {
+    dataToCreate.number = number;
+  }
+
+  if (screenings) {
+    dataToCreate.screenings = {
+      movieId: screenings.movieId,
+      screenId: screenings.screenId,
+      startsAt: screenings.startsAt,
+    };
+  }
+
+  const createdScreen = await prisma.screen.create({
+    data: dataToCreate,
+    include: {
+      screenings: true,
+    },
+  });
+
+  return createdScreen;
+};
+
+module.exports = {
+  getAllScreensDb,
+  createScreenDb,
+};
