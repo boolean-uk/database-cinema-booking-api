edf650eb464bd1231f836d6437a92e1d147d8e80
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-16 12:56:07 +0000

    feat: retrieve movies with runtimes greater and or lesser than given query values

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,8 +1,29 @@
-const { getMoviesDb } = require("../domains/movie");
+const { getMoviesDb, getMoviesWhereAndDb, getMoviesWhereOrDb, getMoviesWhereLtDb, getMoviesWhereGtDb } = require("../domains/movie");
 
 const getMovies = async (req, res) => {
-    const movies = await getMoviesDb()
+    let movies
+    const { runtimeLt, runtimeGt} = req.query
+    if (runtimeLt && runtimeGt) {
+        const runtimeLtNum = Number(runtimeLt)
+        const runtimeGtNum = Number(runtimeGt)
+        movies = await (getMoviesWhereAndDb(runtimeLtNum, runtimeGtNum))
+    }
+    
+    if (runtimeLt) {
+        const runtimeLtNum = Number(runtimeLt)
+        movies = await (getMoviesWhereLtDb(runtimeLtNum))
+    }
+    
+    if (runtimeGt) {
+        const runtimeGtNum = Number(runtimeGt)
+        movies = await (getMoviesWhereGtDb(runtimeGtNum))
+    }
+    
+    if (!runtimeLt && !runtimeGt) {
+        movies = await getMoviesDb()
+    }
     return res.json({ movies: movies })
+    
 }
 
 module.exports = {
--- src/domains/movie.js
+++ src/domains/movie.js
@@ -1,7 +1,43 @@
 const prisma = require("../utils/prisma");
 
 const getMoviesDb = async () => await prisma.movie.findMany();
+const getMoviesWhereGtDb = async (runtimeGt) =>
+  await prisma.movie.findMany({
+    where: {
+      runtimeMins: {
+        gt: runtimeGt,
+      },
+    },
+  });
+const getMoviesWhereLtDb = async (runtimeLt) =>
+  await prisma.movie.findMany({
+    where: {
+      runtimeMins: {
+        lt: runtimeLt,
+      },
+    },
+  });
+const getMoviesWhereAndDb = async (runtimeGt, runtimeLt) =>
+  await prisma.movie.findMany({
+    where: {
+      AND: [
+        {
+          runtimeMins: {
+            lt: runtimeLt,
+          },
+        },
+        {
+          runtimeMins: {
+            gt: runtimeGt,
+          },
+        },
+      ],
+    },
+  });
 
 module.exports = {
   getMoviesDb,
+  getMoviesWhereLtDb,
+  getMoviesWhereGtDb,
+  getMoviesWhereAndDb,
 };
