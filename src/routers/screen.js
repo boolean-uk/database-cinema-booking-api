deaf16bc251fc7ca1a1fa10b79634feec4d5ac79
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 16:23:35 +0000

    FEAT: added POST('/') -createScreen() router.

--- src/routers/screen.js
+++ src/routers/screen.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { createScreen } = require('../controllers/screen.js')
 
-//write screen code here
+router.post('/', createScreen)
 
 module.exports = router
