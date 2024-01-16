716b792000f14e0ddf137bd7162a190196717408
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 12:48:13 +0000

    BUILD: getAllMoviedDB() - returns all movies from DB. Exported to controllers.

--- src/domains/movie.js
+++ src/domains/movie.js
@@ -1,7 +1,8 @@
 const prisma = require('../utils/prisma')
 
+const getAllMoviesDB = async () => await prisma.movie.findMany()
 
 
 module.exports = {
-//  functions for controller page
+    getAllMoviesDB
 }
