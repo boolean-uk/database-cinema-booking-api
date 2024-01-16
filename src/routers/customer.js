67b8d60871b2eae542938e6cb2b6b0c6b53c1a49
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-16 12:48:41 +0000

    style: remove comments

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -4,9 +4,6 @@ const validate = require("../middleware/zod/customer.js");
 
 const router = express.Router();
 
-// In index.js, we told express that the /customer route should use this router file
-// The below /register route extends that, so the end result will be a URL
-// that looks like http://localhost:4040/customer/register
 router.post("/register", validate.createCustomer, createCustomer);
 
 module.exports = router;
