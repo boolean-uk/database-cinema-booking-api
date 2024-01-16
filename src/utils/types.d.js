03f3b58659ea811c4170b0972e80328c5a3ad001
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-16 18:38:54 +0000

    refactor: add typedef

--- src/utils/types.d.js
+++ src/utils/types.d.js
@@ -2,9 +2,10 @@
  * @typedef {import("express").Request} Request
  * @typedef {import("express").Response} Response
  * @typedef {import("express").NextFunction} NextFunction
- * 
+ *
  * @typedef {import("@prisma/client").Customer} Customer
  * @typedef {import("@prisma/client").Contact} Contact
+ * @typedef {import("@prisma/client").Movie} Movie
  */
 const Types = {};
 
