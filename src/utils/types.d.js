b3a397228c09c4ae7be0660a1ffbec03505fdb45
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-16 12:14:40 +0000

    feat: add typedefs

---
+++ src/utils/types.d.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Response} Response
+ * @typedef {import("express").NextFunction} NextFunction
+ */
+const Types = {};
+
+module.exports = Types;
