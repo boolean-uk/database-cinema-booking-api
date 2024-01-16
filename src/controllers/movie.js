04a69543d110cde0a7d8568c3f3299d85287ef5d
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 15:57:21 +0000

    FEAT: add updateMovieById endpoint res.

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,25 +1,37 @@
-const {getAllMoviesDB, createMovieDB, getMovieByIdDB} = require('../domains/movie.js')
+const {
+    getAllMoviesDB,
+    createMovieDB,
+    getMovieByIdDB,
+    updateMovieByIdDB,
+} = require("../domains/movie.js");
 
 const getAllMovies = async (req, res) => {
-    const allMovies = await getAllMoviesDB()
-    res.status(200).json({movies: allMovies})
-}
+    const allMovies = await getAllMoviesDB();
+    res.status(200).json({ movies: allMovies });
+};
 
 const createMovie = async (req, res) => {
-    const {title, runtimeMins} = req.body
-    const newMovie = await createMovieDB(title, runtimeMins)
-    res.status(201).json({movie: newMovie})
-}
+    const { title, runtimeMins } = req.body;
+    const newMovie = await createMovieDB(title, runtimeMins);
+    res.status(201).json({ movie: newMovie });
+};
 
 const getMovieById = async (req, res) => {
-    const id = Number(req.params.id)
-    const foundMovie = await getMovieByIdDB(id)
-    res.status(200).json({movie: foundMovie})
-}
+    const id = Number(req.params.id);
+    const foundMovie = await getMovieByIdDB(id);
+    res.status(200).json({ movie: foundMovie });
+};
 
+const updateMovieById = async (req, res) => {
+    const id = Number(req.params.id);
+    const { title, runtimeMins } = req.body;
+    const updatedMovie = await updateMovieByIdDB(id, title, runtimeMins);
+    res.status(201).json({ movie: updatedMovie });
+};
 
 module.exports = {
     getAllMovies,
     createMovie,
-    getMovieById
-}
+    getMovieById,
+    updateMovieById,
+};
