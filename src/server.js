0bcd9c390bd3e8fbc6d706962fecfb2738a7795c
Kye <NovusAa12@proton.me>
2024-01-16 14:21:23 +0000

    add movieRouter to server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Add middleware
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Tell express to use your routers here
-const customerRouter = require('./routers/customer');
-app.use('/customers', customerRouter);
+const customerRouter = require("./routers/customer");
+app.use("/customers", customerRouter);
 
+const movieRouter = require("./routers/movies");
+app.use("/movies", movieRouter);
 
-module.exports = app
+module.exports = app;
