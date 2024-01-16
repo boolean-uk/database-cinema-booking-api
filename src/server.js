6fc8c58c476ee06eba94e4af62cea65ccb39da95
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 12:24:43 +0000

    BUILD: screen router path.

--- src/server.js
+++ src/server.js
@@ -16,8 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 // Tell express to use your routers here
 const customerRouter = require('./routers/customer');
 const movieRouter = require('./routers/movie.js')
+const screenRouter = require('./routers/screen.js')
 app.use('/customers', customerRouter);
 app.use('/movies', movieRouter)
+app.use('/screen', screenRouter)
+
 
 
 module.exports = app
