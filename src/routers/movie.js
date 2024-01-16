6707911354423e6dfe8a93ace70129f4358c6154
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 12:46:55 +0000

    FIX: changed app for router.

--- src/routers/movie.js
+++ src/routers/movie.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = require("../server");
 const router = express.Router();
 
 const { getAllMovies } = require('../controllers/movie.js')
 
-app.get('/', getAllMovies)
+router.get('/', getAllMovies)
 
 module.exports = router
