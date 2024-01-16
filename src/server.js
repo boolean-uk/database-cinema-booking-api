28944a055eee8059385743f590415af8b0b2c4f5
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 12:21:30 +0000

    BUILD: new movie router.

--- src/server.js
+++ src/server.js
@@ -15,7 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Tell express to use your routers here
 const customerRouter = require('./routers/customer');
+const movieRouter = require('./routers/movie.js')
 app.use('/customers', customerRouter);
+app.use('/movies', movieRouter)
 
 
 module.exports = app
