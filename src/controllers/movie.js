53f95b44c04eaed700515ad6be4d7bdf045311f5
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 12:47:44 +0000

    BUILD: getAllMovies() controller, exported to router.

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,8 +1,12 @@
-//  Write controller here
+const {getAllMoviesDB} = require('../domains/movie.js')
 
+const getAllMovies = async (req, res) => {
 
+    const allMovies = await getAllMoviesDB()
 
+    res.status(200).json({movies: allMovies})
+}
 
 module.exports = {
-    //  functions for router
+    getAllMovies
 }
