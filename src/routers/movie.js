fddb0747028b1a5ba128209f187ceca39418d90a
Lukas Dembicki <lad_777@hotmail.com>
2024-01-16 15:39:49 +0000

    FEAT: added router for getMovieById

--- src/routers/movie.js
+++ src/routers/movie.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllMovies, createMovie } = require('../controllers/movie.js')
+const { getAllMovies, createMovie, getMovieById } = require('../controllers/movie.js')
 
 router.get('/', getAllMovies)
+router.get('/:id', getMovieById)
 router.post('/', createMovie)
+
 
 
 
