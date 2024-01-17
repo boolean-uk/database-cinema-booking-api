f2a488a530d2a4b338fd9e594f31ba280f835c0a
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-17 12:11:56 +0000

    test: add test for create movie with screenings

--- test/api/extensions/movie-ext.spec.js
+++ test/api/extensions/movie-ext.spec.js
@@ -3,12 +3,12 @@ const supertest = require("supertest");
 const { createMovie } = require("../../helpers/createMovie");
 
 describe("Movie Endpoint", () => {
-	beforeEach(async () => {
-		await createMovie("The Fellowship of the Ring", 178);
-		await createMovie("Dodgeball", 120);
-		await createMovie("Scream", 113);
-	})
-  describe("GET/movies?runtimeLt={} and/or runtimeGt={}", () => {
+  xdescribe("GET/movies?runtimeLt={} and/or runtimeGt={}", () => {
+    beforeEach(async () => {
+      await createMovie("The Fellowship of the Ring", 178);
+      await createMovie("Dodgeball", 120);
+      await createMovie("Scream", 113);
+    });
     it("gets only the movies with a runtime lesser than runtimeLt", async () => {
       const response = await supertest(app).get("/movies?runtimeLt=130");
       expect(response.status).toEqual(200);
@@ -18,13 +18,40 @@ describe("Movie Endpoint", () => {
     it("gets only the movies with a runtime greater than runtimeGt", async () => {
       const response = await supertest(app).get("/movies?runtimeGt=115");
       expect(response.status).toEqual(200);
-      expect(response.body.movies[0].title).toEqual("The Fellowship of the Ring");
+      expect(response.body.movies[0].title).toEqual(
+        "The Fellowship of the Ring"
+      );
       expect(response.body.movies[1].title).toEqual("Dodgeball");
     });
     it("gets only the movies with a runtime greater than runtimeGt and lesser than runtimeGt", async () => {
-      const response = await supertest(app).get("/movies?runtimeGt=115&runtimeLt=130");
+      const response = await supertest(app).get(
+        "/movies?runtimeGt=115&runtimeLt=130"
+      );
       expect(response.status).toEqual(200);
       expect(response.body.movies.length).toEqual(1);
     });
+  });
+  describe("POST/movies", () => {});
+  // it("throws a 409 when the title is already in use");
+  // it("throws a 400 error when fields are missing in the request body")
+  it("adds screenings for the movie when these are provided by the CLI", async () => {
+    const request = {
+      title: "Rogue One",
+      runtimeMins: 134,
+      screenings: [
+        {
+          startsAt: "2017-06-11T18:30:00.000Z",
+          screenId: 1,
+        },
+        {
+          startsAt: "2017-08-11T18:30:00.000Z",
+          screenId: 1,
+        },
+      ],
+    };
+    const response = await supertest(app).post("/movies").send(request);
+    expect(response.status).toEqual(201);
+    expect(response.body.movie.screenings).not.toBeUndefined();
+    expect(response.body.movie.screenings.length).toEqual(2);
   });
 });
