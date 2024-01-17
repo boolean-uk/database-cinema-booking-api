3b341723d8437ea2f9d6c1bf1d8dfe13b36fa974
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-17 14:57:27 +0000

    test: add test for 400 on creatMovie()

--- test/api/extensions/movies-ext.spec.js
+++ test/api/extensions/movies-ext.spec.js
@@ -42,9 +42,16 @@ describe("Movie Endpoint", () => {
       const response = await supertest(app).post("/movies").send(request);
       expect(response.status).toEqual(409);
       expect(response.body.error).toEqual("A movie with the provided title already exists")
-
     });
-    // it("throws a 400 error when fields are missing in the request body")
+    it("throws a 400 error when fields are missing in the request body",  async () => {
+      await createMovie("Dodgeball", 120);
+      const request = {
+        runtimeMins: 134,
+      };
+      const response = await supertest(app).post("/movies").send(request);
+      expect(response.status).toEqual(400);
+      expect(response.body.error).toEqual("Missing fiels in request body")
+    })
     it("adds screenings for the movie when these are provided by the CLI", async () => {
       const screen = await createScreen(1);
       const request = {
