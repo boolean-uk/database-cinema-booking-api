c7bc4360144a900da6ea5e731850a5267b95d47f
Satoki <satokiito@gmail.com>
2024-01-17 00:02:21 +0000

    PUT endpoint to update customer, turns out we need the example code for tests to pass!

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -1,5 +1,14 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const { createCustomer, updateCustomer } = require('../controllers/customer.js');
 
+const router = express.Router();
 
-module.exports = router
+// In index.js, we told express that the /customer route should use this router file
+// The below /register route extends that, so the end result will be a URL
+// that looks like http://localhost:4040/customer/register
+router.post("/register", createCustomer);
+
+// UPDATE CUSTOMER BY ID
+router.put('/:id', updateCustomer)
+
+module.exports = router;
