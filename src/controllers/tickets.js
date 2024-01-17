ce5cb3e3534a49162385c31d0b378a3d4d055d08
Kye <NovusAa12@proton.me>
2024-01-17 12:56:22 +0000

    add routes for tickets

---
+++ src/controllers/tickets.js
@@ -0,0 +1,28 @@
+const { findCustomerByIdDb } = require("../domains/customer");
+const { findScreeningByIdDb } = require("../domains/screening");
+const { createTicketDb } = require("../domains/tickets");
+
+const createTicket = async (req, res) => {
+  const { screeningId, customerId } = req.body;
+
+  if (!screeningId || !customerId) {
+    return res.status(400).send({ error: "Missing fields in request body" });
+  }
+
+  const foundCustomer = await findCustomerByIdDb(customerId);
+  const foundScreening = await findScreeningByIdDb(screeningId);
+
+  if (!foundScreening || !foundCustomer) {
+    return res.status(404).send({
+      error: "A customer or screening does not exist with the provided id",
+    });
+  }
+
+  const createdTicket = await createTicketDb(screeningId, customerId);
+
+  return res.status(201).send({ ticket: createdTicket });
+};
+
+module.exports = {
+  createTicket,
+};
---
+++ src/routers/tickets.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createTicket } = require("../controllers/tickets");
+
+const router = express.Router();
+
+router.post("/", createTicket);
+
+module.exports = router;
