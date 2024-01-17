81a90bd9f122ed78e49a8c888f2540ec747f6d2a
Satoki <satokiito@gmail.com>
2024-01-17 00:16:38 +0000

    POST endpoint to create new screen - core complete

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -1,1 +1,12 @@
 const { PrismaClientKnownRequestError } = require("@prisma/client")
+const { createScreenDb } = require('../domains/screen.js')
+
+// CREATE A SCREEN
+
+const createScreen = async (req, res) => {
+    const { number } = req.body
+    const newScreen = await createScreenDb(number)
+    res.status(201).json({ screen: newScreen })
+}
+
+module.exports = { createScreen }
--- src/domains/screen.js
+++ src/domains/screen.js
@@ -1,1 +1,8 @@
 const prisma = require('../utils/prisma')
+
+// CREATE A SCREEN
+const createScreenDb = async (number) => await prisma.screen.create({
+    data: { number }
+})
+
+module.exports = { createScreenDb }
