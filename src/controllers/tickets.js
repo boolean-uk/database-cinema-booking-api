ebf930f320d521c981c4ef33739c99442cec7507
Kye <NovusAa12@proton.me>
2024-01-17 13:03:03 +0000

    add try...catch to tickets controller

--- src/controllers/tickets.js
+++ src/controllers/tickets.js
@@ -3,24 +3,29 @@ const { findScreeningByIdDb } = require("../domains/screening");
 const { createTicketDb } = require("../domains/tickets");
 
 const createTicket = async (req, res) => {
-  const { screeningId, customerId } = req.body;
+  try {
+    const { screeningId, customerId } = req.body;
 
-  if (!screeningId || !customerId) {
-    return res.status(400).send({ error: "Missing fields in request body" });
+    if (!screeningId || !customerId) {
+      return res.status(400).send({ error: "Missing fields in request body" });
+    }
+
+    const foundCustomer = await findCustomerByIdDb(customerId);
+    const foundScreening = await findScreeningByIdDb(screeningId);
+
+    if (!foundScreening || !foundCustomer) {
+      return res.status(404).send({
+        error: "A customer or screening does not exist with the provided id",
+      });
+    }
+
+    const createdTicket = await createTicketDb(screeningId, customerId);
+
+    return res.status(201).send({ ticket: createdTicket });
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ error: e.message });
   }
-
-  const foundCustomer = await findCustomerByIdDb(customerId);
-  const foundScreening = await findScreeningByIdDb(screeningId);
-
-  if (!foundScreening || !foundCustomer) {
-    return res.status(404).send({
-      error: "A customer or screening does not exist with the provided id",
-    });
-  }
-
-  const createdTicket = await createTicketDb(screeningId, customerId);
-
-  return res.status(201).send({ ticket: createdTicket });
 };
 
 module.exports = {
