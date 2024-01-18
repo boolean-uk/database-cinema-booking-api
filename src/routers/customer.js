f66e14a9431c3718224a7deb9bccf50bf20bda5a
Loza_TM <lozatmamo@gmail.com>
2024-01-18 16:36:55 +0100

    Set up Express router for customer routes

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require("express")
 const {
-  createCustomer
-} = require('../controllers/customer');
+  createCustomer,
+  updateCustomerById,
+} = require('../controllers/customer')
 
-const router = express.Router();
+const router = express.Router()
 
 // In index.js, we told express that the /customer route should use this router file
 // The below /register route extends that, so the end result will be a URL
 // that looks like http://localhost:4040/customer/register
-router.post("/register", createCustomer);
+//router.post("/register", createCustomer);
 
-module.exports = router;
+//a route for updating a customer by ID using the HTTP PUT method.
+router.put("/:id", updateCustomerById)
+
+module.exports = router
