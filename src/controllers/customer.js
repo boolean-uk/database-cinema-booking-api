0a2d1fd6c68a02426e0ee179b48cc715a3ea787b
Lukas Dembicki <lad_777@hotmail.com>
2024-01-19 13:48:03 +0000

    FEAT: now handles updating contact as well. More error handling.

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -13,32 +13,16 @@ const createCustomer = async (req, res) => {
         });
     }
 
-    // Try-catch is a very common way to handle errors in JavaScript.
-    // It allows us to customise how we want errors that are thrown to be handled.
-    // Read more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-
-    // Here, if Prisma throws an error in the process of trying to create a new customer,
-    // instead of the Prisma error being thrown (and the app potentially crashing) we exit the
-    // `try` block (bypassing the `res.status` code) and enter the `catch` block.
     try {
         const createdCustomer = await createCustomerDb(name, phone, email);
 
         res.status(201).json({ customer: createdCustomer });
-    } catch (e) {
-        // In this catch block, we are able to specify how different Prisma errors are handled.
-        // Prisma throws errors with its own codes. P2002 is the error code for
-        // "Unique constraint failed on the {constraint}". In our case, the {constraint} is the
-        // email field which we have set as needing to be unique in the prisma.schema.
-        // To handle this, we return a custom 409 (conflict) error as a response to the client.
-        // Prisma error codes: https://www.prisma.io/docs/orm/reference/error-reference#common
-        // HTTP error codes: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
-        if (e instanceof PrismaClientKnownRequestError) {
-            if (e.code === "P2002") {
-                return res
-                    .status(409)
-                    .json({
-                        error: "A customer with the provided email already exists",
-                    });
+    } catch (err) {
+        if (err instanceof PrismaClientKnownRequestError) {
+            if (err.code === "P2002") {
+                return res.status(409).json({
+                    error: "A customer with the provided email already exists",
+                });
             }
         }
 
@@ -48,9 +32,30 @@ const createCustomer = async (req, res) => {
 
 const updateCustomerById = async (req, res) => {
     const id = Number(req.params.id);
-    const { name } = req.body;
-    const updatedCustomer = await updateCustomerByIdDB(id, name);
-    res.status(201).json({ customer: updatedCustomer });
+    const { name, contact } = req.body;
+
+    if (!name) {
+        return res.status(400).json({
+            error: "Missing fields in request body",
+        });
+    }
+
+    try {
+        const updatedCustomer = await updateCustomerByIdDB(id, name, contact);
+        res.status(201).json({ customer: updatedCustomer });
+
+    } catch (err) {
+        if (err instanceof PrismaClientKnownRequestError) {
+            if (err.code === "P2025") {
+                return res
+                    .status(404)
+                    .json({ error: `Customer with id ${id} not found` });
+            }
+        }
+        console.log(err.message)
+        res.status(500).json({ error: err.message });
+
+    }
 };
 
 module.exports = {
