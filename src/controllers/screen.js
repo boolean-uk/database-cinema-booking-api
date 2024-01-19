efc8d24a40b50c6df9caa449c5fd63e7a576c6a9
Lukas Dembicki <lad_777@hotmail.com>
2024-01-19 17:15:38 +0000

    FEAT: error handling

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -1,10 +1,30 @@
+const {
+    PrismaClientKnownRequestError,
+} = require("@prisma/client/runtime/library.js");
 const { createScreenDB } = require("../domains/screen.js");
 
 const createScreen = async (req, res) => {
-    const { number } = req.body;
-    const newScreen = await createScreenDB(number);
-    console.log(newScreen);
-    res.status(201).json({ screen: newScreen });
+    const { number, screenings } = req.body;
+
+    if (!screenings.startsAt) {
+        res.status(400).json({
+            error: `Missing fields in request body`,
+        });
+    }
+    try {
+        const newScreen = await createScreenDB(number, screenings);
+        res.status(201).json({ screen: newScreen });
+    } catch (err) {
+        if (err instanceof PrismaClientKnownRequestError) {
+            if (err.code === "P2002") {
+                return res.status(409).json({
+                    error: `A screen with the number ${number} already exists`,
+                });
+            }
+        }
+        console.log(err.message)
+        res.status(500).json({ error: err.message });
+    }
 };
 
 module.exports = {
