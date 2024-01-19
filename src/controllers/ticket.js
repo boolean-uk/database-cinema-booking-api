6e4bce2596c54dfb8d9ac8cef18d90602006120a
Lukas Dembicki <lad_777@hotmail.com>
2024-01-19 17:35:14 +0000

    BUILD: ticket controller with error handling.

---
+++ src/controllers/ticket.js
@@ -0,0 +1,31 @@
+const { PrismaClientKnownRequestError } = require("@prisma/client/runtime/library.js");
+
+const { createTicketDB } = require("../domains/ticket.js");
+
+const createTicket = async (req, res) => {
+    const { screeningId, customerId } = req.body;
+
+    if (!screeningId || !customerId) {
+        res.status(400).json({
+            error: `Missing fields in request body`,
+        });
+    }
+    try {
+        const newTicket = await createTicketDB(screeningId, customerId);
+        res.status(201).json({ ticket: newTicket });
+    } catch (err) {
+        if (err instanceof PrismaClientKnownRequestError) {
+            if (err.code === "P2002") {
+                return res.status(409).json({
+                    error: `A customer or screening does not exist with the provided id`,
+                });
+            }
+        }
+        console.log(err.message);
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = {
+    createTicket,
+};
