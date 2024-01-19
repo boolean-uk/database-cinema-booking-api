cf850acd1c41c614964d82a03a8482723a70c0c8
Lukas Dembicki <lad_777@hotmail.com>
2024-01-19 13:49:03 +0000

    FEAT: query now checks for contact information and updates accordingly.

--- src/domains/customer.js
+++ src/domains/customer.js
@@ -1,9 +1,5 @@
 const prisma = require("../utils/prisma");
 
-/**
- * This will create a Customer AND create a new Contact, then automatically relate them with each other
- * @tutorial https://www.prisma.io/docs/concepts/components/prisma-client/relation-queries#create-a-related-record
- */
 const createCustomerDb = async (name, phone, email) =>
     await prisma.customer.create({
         data: {
@@ -15,20 +11,27 @@ const createCustomerDb = async (name, phone, email) =>
                 },
             },
         },
-        // We add an `include` outside of the `data` object to make sure the new contact is returned in the result
-        // This is like doing RETURNING in SQL
         include: {
             contact: true,
         },
     });
 
-const updateCustomerByIdDB = async (id, name) =>
+const updateCustomerByIdDB = async (id, name, contact) =>
     await prisma.customer.update({
         where: {
             id,
         },
         data: {
             name,
+            contact: contact && {
+                delete: {
+                    customerId: id,
+                },
+                create: {
+                    phone: contact.phone,
+                    email: contact.email,
+                },
+            },
         },
         include: {
             contact: true,
