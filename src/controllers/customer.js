b3d2d448bcc01a532691e1380a1df52ff7feb5c2
SukunimiVinod <Vinodvinod19032022@gmail.com>
2024-01-22 20:02:15 +0200

    added Endpoint to update customer by Id

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -1,17 +1,17 @@
-const { PrismaClientKnownRequestError } = require("@prisma/client")
-const { createCustomerDb } = require('../domains/customer.js')
+const { PrismaClientKnownRequestError } = require("@prisma/client");
+const {
+  createCustomerDb,
+  updateCustomerDb,
+} = require("../domains/customer.js");
+const { updateMovieDb } = require("../domains/movie.js");
 
 const createCustomer = async (req, res) => {
-  const {
-    name,
-    phone,
-    email
-  } = req.body
+  const { name, phone, email } = req.body;
 
   if (!name || !phone || !email) {
     return res.status(400).json({
-      error: "Missing fields in request body"
-    })
+      error: "Missing fields in request body",
+    });
   }
 
   // Try-catch is a very common way to handle errors in JavaScript.
@@ -22,9 +22,9 @@ const createCustomer = async (req, res) => {
   // instead of the Prisma error being thrown (and the app potentially crashing) we exit the
   // `try` block (bypassing the `res.status` code) and enter the `catch` block.
   try {
-    const createdCustomer = await createCustomerDb(name, phone, email)
+    const createdCustomer = await createCustomerDb(name, phone, email);
 
-    res.status(201).json({ customer: createdCustomer })
+    res.status(201).json({ customer: createdCustomer });
   } catch (e) {
     // In this catch block, we are able to specify how different Prisma errors are handled.
     // Prisma throws errors with its own codes. P2002 is the error code for
@@ -35,14 +35,23 @@ const createCustomer = async (req, res) => {
     // HTTP error codes: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
     if (e instanceof PrismaClientKnownRequestError) {
       if (e.code === "P2002") {
-        return res.status(409).json({ error: "A customer with the provided email already exists" })
+        return res
+          .status(409)
+          .json({ error: "A customer with the provided email already exists" });
       }
     }
 
-    res.status(500).json({ error: e.message })
+    res.status(500).json({ error: e.message });
   }
-}
+};
 
+const updateCustomer = async (req, res) => {
+  const id = Number(req.params.id);
+  const { name } = req.body;
+  const updatedCustomer = await updateCustomerDb(id,name);
+  res.status(201).json({ customer: updatedCustomer });
+};
 module.exports = {
-  createCustomer
-}
+  createCustomer,
+  updateCustomer,
+};
