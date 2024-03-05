6e25501815977ff0c97cb356bfc1e5074195ea6f
Radio58 <rt58gm@gmail.com>
2024-03-05 12:47:15 +0000

    customer cleanup

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const router = express.Router();
+
 const {
   createCustomer,
   updateCustomer
 } = require('../controllers/customer');
 
-const router = express.Router();
 
 // In index.js, we told express that the /customer route should use this router file
 // The below /register route extends that, so the end result will be a URL
