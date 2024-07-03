da78c764d94dc6f220f71f2ba0d84d0fbda6bbc8
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-07-03 13:11:08 +0100

    creation of movie domain

---
+++ src/domains/movie.js
@@ -0,0 +1,36 @@
+const prisma = require("../utils/prisma");
+
+const all = async () => await prisma.movie.findMany();
+const getById = async (id) => {
+  const result = await prisma.movie.findUnique({
+    where: { id: id },
+  });
+  return result;
+};
+const getByTitle = async (title) => {
+  const result = await prisma.movie.findUnique({
+    where: {
+      title: title,
+    },
+  });
+  return result;
+};
+const create = async (title, runtimeMins) => {
+  const result = await prisma.movie.create({
+    data: {
+      title,
+      runtimeMins,
+    },
+  });
+  return result;
+};
+const update = async () => {};
+const remove = async () => {};
+module.exports = {
+  all,
+  getById,
+  getByTitle,
+  create,
+  update,
+  remove,
+};
