fdb795ac949e1b7ce09e0d024e6d1b10cc2ae84a
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-07-03 12:56:56 +0100

    creating movie controllers

---
+++ src/controllers/movie.js
@@ -0,0 +1,12 @@
+const all = async (req, res) => {};
+const create = async (req, res) => {};
+const get = async (req, res) => {};
+const update = async (req, res) => {};
+const remove = async (req, res) => {};
+module.exports = {
+  all,
+  create,
+  get,
+  update,
+  remove,
+};
